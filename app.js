4218a3ee25ecd3c5aba821f77b9c7e7f0ef85153
NENG0910 <85937816+NENG0910@users.noreply.github.com>
2022-05-27 18:04:30 +0800

    load data

--- app.js
+++ app.js
@@ -39,6 +39,16 @@ add.addEventListener("click", (e) => {
   trashButton.addEventListener("click", (e) => {
     let todoItem = e.target.parentElement;
     todoItem.addEventListener("animationend", () => {
+      //remove from loaclstorage
+      let text = todoItem.children[0].innerText;
+      let myListArray = JSON.parse(localStorage.getItem("list"));
+
+      myListArray.forEach((item, index) => {
+        if (item.todoText == text) {
+          myListArray.splice(index, 1);
+          localStorage.setItem("list", JSON.stringify(myListArray));
+        }
+      });
       todoItem.remove();
     });
     todoItem.style.animation = "scaleDown 0.3s forwards";
@@ -67,8 +77,62 @@ add.addEventListener("click", (e) => {
 
   console.log(JSON.parse(localStorage.getItem("list")));
 
-  console.log(myTodo);
-
   section.appendChild(todo);
   form.children[0].value = "";
 });
+
+//Load data from localstorage
+let myList = localStorage.getItem("list");
+{
+  if (myList !== null) {
+    let myListArray = JSON.parse(myList);
+    myListArray.forEach((item) => {
+      let todo = document.createElement("div");
+      todo.classList.add("todo");
+      let text = document.createElement("p");
+      text.classList.add("todo-text");
+      text.innerText = item.todoText;
+      let time = document.createElement("p");
+      time.classList.add("todo-time");
+      time.innerText = item.todoMonth + "/" + item.todoDate;
+      todo.appendChild(text);
+      todo.appendChild(time);
+
+      //completeButton
+      let completeButoon = document.createElement("button");
+      completeButoon.classList.add("complete");
+      completeButoon.innerHTML = '<i class="fa-solid fa-check"></i>';
+      completeButoon.addEventListener("click", (e) => {
+        let todoItem = e.target.parentElement;
+        todoItem.classList.toggle("done");
+      });
+
+      //trashButton
+      let trashButton = document.createElement("button");
+      trashButton.classList.add("trash");
+      trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+      trashButton.addEventListener("click", (e) => {
+        let todoItem = e.target.parentElement;
+        todoItem.addEventListener("animationend", () => {
+          //remove from loaclstorage
+          let text = todoItem.children[0].innerText;
+          let myListArray = JSON.parse(localStorage.getItem("list"));
+
+          myListArray.forEach((item, index) => {
+            if (item.todoText == text) {
+              myListArray.splice(index, 1);
+              localStorage.setItem("list", JSON.stringify(myListArray));
+            }
+          });
+
+          todoItem.remove();
+        });
+        todoItem.style.animation = "scaleDown 0.3s forwards";
+      });
+
+      todo.appendChild(completeButoon);
+      todo.appendChild(trashButton);
+      section.append(todo);
+    });
+  }
+}
